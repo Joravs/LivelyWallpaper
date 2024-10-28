965018c352fa66453fe13b695353dad37ee3e61e
Jordy <jordyl.reyesl@gmail.com>
2024-10-28 09:09:34 +0100

    c

--- wallpapers/Test/imgcenter/audiocurrent.js
+++ wallpapers/Test/imgcenter/audiocurrent.js
@@ -3,14 +3,27 @@ const middle = document.getElementById("middle");
 function livelyCurrentTrack(data) {
     let obj = JSON.parse(data);
     //when no track is playing its null
-    if (obj != null){
-        console.log(obj.Title);
-        if (obj.Thumbnail != null){
-            const base64String = !obj.Thumbnail.startsWith("data:image/")
-            ? "data:image/png;base64," + obj.Thumbnail
-            : obj.Thumbnail;
+    if (obj != null) {
+        document.querySelector("h2").innerText = obj.AlbumArtist;
+        document.querySelector("h2").innerText = obj.Artist;
+        document.querySelector("h1").innerText = obj.Title;
+        $("#backgroundImageFade").css("opacity", 1.0);
+        backgroundImageFade.src = backgroundImage.src;
+        if (obj.Thumbnail != null) {
+          document.getElementById("albumart").src = "data:image/png;base64, " + obj.Thumbnail;
+          backgroundImage.src = "data:image/png;base64, " + obj.Thumbnail;
+        } else {
+          document.getElementById("albumart").src = defaultAlbumArt;
+          backgroundImage.src = null;
         }
-    }
+      } else {
+        document.querySelector("h2").innerText = "";
+        document.querySelector("h1").innerText = "Waiting for media...";
+        document.getElementById("albumart").src = defaultAlbumArt;
+        $("#backgroundImageFade").css("opacity", 1.0);
+        backgroundImageFade.src = backgroundImage.src;
+        backgroundImage.src = defaultBackground;
+      }
 }
 
 document.addEventListener("DOMContentLoaded",function(){
