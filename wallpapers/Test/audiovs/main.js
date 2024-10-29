698463cf91199b43875e64ac18f9f5fff3827bf1
Joravs <jordyl.reyesl@gmail.com>
2024-10-29 22:54:45 +0100

    --- Auto Git Commit ---

--- wallpapers/Test/audiovs/main.js
+++ wallpapers/Test/audiovs/main.js
@@ -77,6 +77,11 @@ function livelyAudioListener(audioArray) {
     shadowOpacity / 100
   }))`
 
+  /////
+  for (let i = 1; i <=36; i++) {
+    middle.style.transform = "rotate("+i*10+"deg)";
+  }
+
   if (audio.length > 2) {
     // Draw each bar
     audio.map((val, index, arr) => {
