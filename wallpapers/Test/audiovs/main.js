1e9c15777dce63c9b29634efd3f787096b2b27f0
Joravs <jordyl.reyesl@gmail.com>
2024-10-29 22:30:32 +0100

    --- Auto Git Commit ---

--- wallpapers/Test/audiovs/main.js
+++ wallpapers/Test/audiovs/main.js
@@ -114,6 +114,9 @@ function livelyCurrentTrack(data) {
   if (obj != null) {
       if (obj.Thumbnail != null) {
         middle.src = "data:image/png;base64, "  + obj.Thumbnail;
+        for (let i = 0; i <36 ; i++) {
+          middle.src.style.transform = `rotate(${i*10}deg)`;
+        }
       } else {
         middle.src = defaultAlbumArt;
         // backgroundImage.src = null;
