a2fdc6780b39983633256b4d753500e59fb41138
Joravs <jordyl.reyesl@gmail.com>
2024-11-05 21:31:15 +0100

    --- Auto Git Commit ---

--- mwhs/php.php
+++ mwhs/php.php
@@ -1,22 +1,20 @@
 <?php
     function getElements($url,$selector){
         $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $html = curl_exec($ch);
-    curl_close($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $html = curl_exec($ch);
+        curl_close($ch);
 
-    // Utilizar   
- un parser HTML para extraer el elemento
-    $dom = new DOMDocument();
-    @$dom->loadHTML($html);
-    $xpath = new DOMXPath($dom);
-    $elements = $xpath->query($selector);
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+        $xpath = new DOMXPath($dom);
+        $elements = $xpath->query($selector);
 
-    if ($elements->length > 0) {
-        return $elements->item(0)->nodeValue;
-    } else {
-        return "Elemento no encontrado";
-    }
+        if ($elements->length > 0) {
+            return $elements->item(0)->nodeValue;
+        } else {
+            return "Elemento no encontrado";
+        }
     }
 ?>
